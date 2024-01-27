0c690b4e145d302f3163368075fc3ff31b2a1f49
Nur Chamdani <kokme.km@gmail.com>
2024-01-27 19:16:00 +0700

    update
    add files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Portofolio</title>
-    <link rel="stylesheet" href="{{ URL::to('/') }}/css/app.css">
+    <link rel="stylesheet" href="{{ URL::to('/') }}/style.css">
 </head>
 <body>
     <nav id="desktop_nav">
@@ -16,6 +16,22 @@
                 <li><a href="#about">Project</a></li>
                 <li><a href="#about">Content</a></li>
             </ul>
+        </div>
+    </nav>
+    <nav id="hamburger-nav">
+        <div class="logo"></div>
+        <div class="hamburger-menu">
+            <div class="hamburger-icon" onclick="toggleMenu()">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div class="menu-links">
+                <li><a href="#about" onclick="toggleMenu()">About</a></li>
+                <li><a href="#about" onclick="toggleMenu()">Experience</a></li>
+                <li><a href="#about" onclick="toggleMenu()">Project</a></li>
+                <li><a href="#about" onclick="toggleMenu()">Content</a></li>
+            </div>
         </div>
     </nav>
     <script src="script.js"></script>
