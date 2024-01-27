caa2093b310ca2437558d6a003d0b0275be67050
Nur Chamdani <kokme.km@gmail.com>
2024-01-27 17:22:54 +0700

    update
    update home

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/../domains/jurnal.live/root_apk/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -31,7 +31,7 @@ if (file_exists($maintenance = __DIR__.'/../domains/jurnal.live/root_apk/storage
 |
 */
 
-require __DIR__.'/../domains/jurnal.live/root_apk/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +44,7 @@ require __DIR__.'/../domains/jurnal.live/root_apk/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../domains/jurnal.live/root_apk/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
