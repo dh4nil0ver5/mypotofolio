b8b91f6e6e3138f7e3fd279b4ddce8a03c252689
Nur Chamdani <kokme.km@gmail.com>
2024-01-28 23:46:13 +0700

    update file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,32 +36,153 @@
         </div>
     </nav>
     <!-- Desktop -->
-    <section id="profile">
-        <div class="section__pic-container"> 
-            <img src="{{ URL::to('/') }}/pic/circle_photo.png" alt="Nur Chamdani" >
-        </div>  
-        <div class="section__text">
-            <p class="section__text__p1" background="red">Hello, I'm</p>
-            <h1 class="title">Nur Chamdani </h1>
-            <p class="setion__text__p2">Fullstack Developer</p>
-            <div class="btn-container">
-                <button class="btn btn-color-2" onclick="" >Download CV</button>
-                <button class="btn btn-color-1" onclick="" >Contact Person</button>
+    <div id="pages">
+        <section id="profile">
+            <div class="section__pic-container"> 
+                <img src="{{ URL::to('/') }}/pic/circle_photo.png" alt="Nur Chamdani" >
+            </div>  
+            <div class="section__text">
+                <p class="section__text__p1" background="red">Hello, I'm</p>
+                <h1 class="title">Nur Chamdani </h1>
+                <p class="setion__text__p2">Fullstack Developer</p>
+                <div class="btn-container">
+                    <button class="btn btn-color-2" onclick="" >Download CV</button>
+                    <button class="btn btn-color-1" onclick="" >Contact Person</button>
+                </div>
+                <div id="social-container">
+                    <img class="pass-img" src="{{ URL::to('/') }}/pic/linkedin.png" alt="LinkedIn" onclick="location.href=''">
+                    <img class="pass-img" src="{{ URL::to('/') }}/pic/github.png" alt="Github" onclick="location.href=''">
+                </div>
+            </div> 
+        </section>
+        <section id="profile_mob">
+            <div class="pic_container">
+                <img src="{{ URL::to('/') }}/pic/circle_photo.png" alt="Nur Chamdani" >
             </div>
-            <div id="social-container">
-                <img class="pass-img" src="{{ URL::to('/') }}/pic/linkedin.png" alt="LinkedIn" onclick="location.href=''">
-                <img class="pass-img" src="{{ URL::to('/') }}/pic/github.png" alt="Github" onclick="location.href=''">
+            <div class="Info">
+                <p class="section__text__p1" background="red">Hello, I'm</p>
+                <h1 class="title">Nur Chamdani </h1>
+                <p class="setion__text__p2">Fullstack Developer</p>
+                <div class="btn-container">
+                    <button class="btn btn-color-2" onclick="" >Download CV</button>
+                    <button class="btn btn-color-1" onclick="" >Contact Person</button>
+                </div>
+                <div id="social-container">
+                    <img class="pass-img" src="{{ URL::to('/') }}/pic/linkedin.png" alt="LinkedIn" onclick="location.href=''">
+                    <img class="pass-img" src="{{ URL::to('/') }}/pic/github.png" alt="Github" onclick="location.href=''">
+                </div>
             </div>
-        </div> 
-    </section>
-    <section id="profile_mob">
-        <div class="pic_container">
-            Test
-        </div>
-        <div class="Info">
-            Test
-        </div>
-    </section>
+        </section>
+        <section id="about_mob"> 
+            <div class="pic_container">
+                <h4>About</h4>
+            </div>
+            <div class="about_detail">
+                <div class="pic_about">
+                    <img src="{{ URL::to('/') }}/pic/circle_photo.png" alt="Nur Chamdani" >
+                </div>
+                <div class="about_myself">
+                    <p>
+                    I am a full stack developer. Experienced in the field of Backend and Frontend development as a freelancer. 
+                    Has worked as a contract employee at PT Sinergi Informatika Semen Indonesia Tbk with work assignments to assist 
+                    with development in Frontend using Codeigniter and Backend using Oracle. 
+                    Has been a teacher for 3 years, namely a software educator Teacher at Dharma Wanita High Vocanary School Gresik. 
+                    Has worked as a Running Support User for fixing data using AWS Redshift, 
+                    Troubleshooting PowerBi and monitoring jobs at a vendor company with the client company 
+                    PT Inovasi Optima Visitama by Vendor of PT Sampoerna Tbk (Phillip Morris International). 
+                    </p>
+                </div>
+            </div> 
+        </section>
+        <section id="experience_mob"> 
+            <div class="pic_container">
+                <h4>Experience</h4>
+            </div>
+            <div class="experience">
+                <div class="list">
+                  <div class="head_list">
+                    <h5>Beckend</h5>
+                  </div>
+                  <div class="detail_list">
+                    <div class="field_list">
+                        <div class="icon_veryfied">
+                            veryf
+                        </div>
+                        <div class="box_skill_detail">
+                            <h3 class="name_skill">MYSQL</h3>
+                            <h6 class="exp_year">Experienced 4 year</h6>
+                        </div>
+                    </div>
+                    <div class="field_list">
+                        <div class="icon_veryfied">
+                            veryf
+                        </div>
+                        <div class="box_skill_detail">
+                            <h3 class="name_skill">Oracle</h3>
+                            <h6 class="exp_year">Experienced 1 year</h6>
+                        </div>
+                    </div>
+                    <div class="field_list">
+                        <div class="icon_veryfied">
+                            veryf
+                        </div>
+                        <div class="box_skill_detail">
+                            <h3 class="name_skill">HTML</h3>
+                            <h6 class="exp_year">Experienced 4 year</h6>
+                        </div>
+                    </div>
+                    <div class="field_list">
+                        <div class="icon_veryfied">
+                            veryf
+                        </div>
+                        <div class="box_skill_detail">
+                            <h3 class="name_skill">Javascript</h3>
+                            <h6 class="exp_year">Experienced 4 year</h6>
+                        </div>
+                    </div>
+                    <div class="field_list">
+                        <div class="icon_veryfied">
+                            veryf
+                        </div>
+                        <div class="box_skill_detail">
+                            <h3 class="name_skill">PHP</h3>
+                            <h6 class="exp_year">Experienced 4 year</h6>
+                        </div>
+                    </div>
+                    <div class="field_list">
+                        <div class="icon_veryfied">
+                            veryf
+                        </div>
+                        <div class="box_skill_detail">
+                            <h3 class="name_skill">SASS / CSS Stylesheet</h3>
+                            <h6 class="exp_year">Experienced 4 year</h6>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="list">
+                    <div class="head_list">
+                        <h5>Frontend</h5>
+                    </div>  
+                    <div class="detail_list">
+                      Android Native, 
+                      Flutter, 
+                      React Native
+                    </div>
+                </div>
+            </div> 
+        </section>
+        <section id="project_mob"> 
+            <div class="pic_container">
+                <h4>Project</h4>
+            </div>
+            <div class="experience">
+            </div> 
+        </section>
+    </div>
+    <div id="arrows">
+        <div class="feedback">v</div> 
+    </div>
     <script src="script.js"></script>
 </body>
 </html>
